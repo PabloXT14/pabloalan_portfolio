864d1cf2c9271d6c54dc628336997bdff4ed4821
Pablo Alan <pabloxt14@gmail.com>
2023-02-28 18:19:59 -0300

    feat: setuping front-end web

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import abouts from "./abouts";
+import brands from "./brands";
+import contacts from "./contacts";
+import experiences from "./experiences";
+import skills from "./skills";
+import testimonials from "./testimonials";
+import workExperience from "./workExperience";
+import works from "./works";
+
+export const schemaTypes = [
+  works,
+  testimonials,
+  brands,
+  abouts,
+  skills,
+  workExperience,
+  experiences,
+  contacts
+];
