5aa60782c439c3562d00c241205a958f420ed66d
Pablo Alan <pabloxt14@gmail.com>
2023-03-31 16:06:16 -0300

    feat: styling work item in work section

--- web/src/containers/Work/index.tsx
+++ web/src/containers/Work/index.tsx
@@ -46,8 +46,6 @@ const Work = () => {
     setActiveFilter(option);
   }
 
-  // console.log(filterWork);
-
   return (
     <div className="flex flex-col items-center justify-center flex-1 w-full">
       <h2 className={styles.headText}>My Creative <span>Portfolio</span> Section</h2>
@@ -60,10 +58,10 @@ const Work = () => {
             className={clsx(
               styles.pText,
               styles.appFlex,
-              'py-2 px-4 rounded-lg bg-white text-zinc-900 font-extrabold cursor-pointer m-2 transition-all duration-300 ease-in-out',
+              'py-2 px-4 rounded-lg font-extrabold cursor-pointer m-2 transition-all duration-300 ease-in-out',
               'hover:bg-secondary hover:text-white',
               'lg:py-4 lg:px-8 lg:rounded-xl',
-              activeFilter === option ? 'text-white bg-secondary' : '',
+              activeFilter === option ? 'bg-secondary text-white' : 'bg-white text-zinc-900',
             )}
           >
             {option}
@@ -75,28 +73,41 @@ const Work = () => {
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className={clsx(
-          ''
+          'flex flex-wrap justify-center items-center',
         )}
       >
         {filterWork.map((work, index) => (
           <div
             key={index}
-            className={clsx('flex justify-center items-center')}
+            className={clsx(
+              styles.appFlex,
+              'w-[270px] flex-col m-8 p-4 rounded-lg bg-white text-zinc-900 cursor-pointer',
+              'transition-all duration-300 ease-in-out',
+              'hover:shadow-work-item',
+              'lg:w-[470px] lg:p-5 lg:rounded-xl',
+              'max-sm:w-full max-sm:m-4'
+            )}
           >
-            <div className={clsx('flex justify-center items-center')}>
+            <div className={clsx(
+              styles.appFlex,
+              'w-full h-[230px] relative',
+              'lg:h-[350px]'
+            )}>
               <Image
                 src={work.imgUrl}
                 alt={work.title}
                 width={1200}
                 height={900}
-                className={clsx('')}
+                className={clsx('w-full h-full object-cover rounded-lg')}
               />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className={clsx(
-                  'flex justify-center items-center'
+                  styles.appFlex,
+                  'absolute inset-0 w-full h-full bg-zinc-900/50 rounded-lg opacity-0',
+                  'transition-all duration-300 ease-in-out',
                 )}
               >
                 <Link
@@ -109,9 +120,15 @@ const Work = () => {
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
-                    className={clsx('flex justify-center items-center')}
+                    className={clsx(
+                      styles.appFlex,
+                      'w-[50px] h-[50px] rounded-full bg-zinc-900/50 text-white m-4 font-sans font-extrabold cursor-pointer',
+                      'transition-all duration-300 ease',
+                    )}
                   >
-                    <AiFillEye />
+                    <AiFillEye
+                      className="w-1/2 text-white h-1/2"
+                    />
                   </motion.div>
                 </Link>
 
@@ -125,9 +142,15 @@ const Work = () => {
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
-                    className={clsx('flex justify-center items-center')}
+                    className={clsx(
+                      styles.appFlex,
+                      'w-[50px] h-[50px] rounded-full bg-zinc-900/50 text-white m-4 font-sans font-extrabold cursor-pointer',
+                      'transition-all duration-300 ease',
+                    )}
                   >
-                    <AiFillGithub />
+                    <AiFillGithub
+                      className="w-1/2 text-white h-1/2"
+                    />
                   </motion.div>
                 </Link>
               </motion.div>
