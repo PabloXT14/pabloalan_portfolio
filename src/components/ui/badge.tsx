bf900d635ac53bc36e05019243e280342dd84b07
Pablo Alan <pabloxt14@gmail.com>
2024-02-13 20:07:30 -0300

    feat: create badge component

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
+
+import { cn } from '@/lib/utils'
+
+const badgeVariants = cva(
+  'flex items-center justify-center rounded-lg px-3 py-1',
+  {
+    variants: {
+      variant: {
+        default: 'bg-sky-900/80 text-sm text-sky-400',
+        secondary:
+          'border-transparent bg-gray-100 text-gray-900 hover:bg-gray-100/80 dark:bg-gray-800 dark:text-gray-50 dark:hover:bg-gray-800/80',
+        destructive:
+          'border-transparent bg-red-500 text-gray-50 hover:bg-red-500/80 dark:bg-red-900 dark:text-gray-50 dark:hover:bg-red-900/80',
+        outline: 'text-sky-400 border border-sky-400',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+)
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  )
+}
+
+export { Badge, badgeVariants }
