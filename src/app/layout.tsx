982d36d049aeedfd3dbf12eb15021dba35472aca
Pablo Alan <pabloxt14@gmail.com>
2024-02-13 15:35:22 -0300

    feat: initial setup to new portfolio design

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next'
+import { Inter, IBM_Plex_Mono } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+})
+
+const plexMono = IBM_Plex_Mono({
+  variable: '--font-plex-mono',
+  subsets: ['latin'],
+  weight: ['400', '500'],
+})
+
+export const metadata: Metadata = {
+  title: 'Portfolio - Pablo Alan',
+  description: 'Meu portfólio pessoal contendo informações e projetos',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${inter.className} ${plexMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['var(--font-inter)', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('/images/hero-bg-02.png')",
+      },
+      fontFamily: {
+        mono: ['var(--font-plex-mono)', 'monospace'],
+      },
+      boxShadow: {
+        button: '0px 0px 68px 7px rgba(2, 132, 199, 0.4)',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
