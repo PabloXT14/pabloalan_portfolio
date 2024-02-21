0d4130405aa0de8929ce276691c16042be52f2f4
Pablo Alan <pabloxt14@gmail.com>
2024-02-20 21:59:13 -0300

    feat: create base structure highlighted-project-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { HeroSection } from '@/components/pages/home/hero-section'
+import { HighlightedProjectsSection } from '@/components/pages/home/highlighted-projects-section'
 import { KnownTechsSection } from '@/components/pages/home/known-techs-section'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <HeroSection />
       <KnownTechsSection />
+      <HighlightedProjectsSection />
     </>
   )
 }
---
+++ src/components/pages/home/highlighted-projects-section/index.tsx
@@ -0,0 +1,9 @@
+import { SectionTitle } from '@/components/section-title'
+
+export const HighlightedProjectsSection = () => {
+  return (
+    <div className="container py-16">
+      <SectionTitle subtitle="destaques" title="Projetos em Destaque" />
+    </div>
+  )
+}
