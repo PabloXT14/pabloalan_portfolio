e63935c0eeac79f55527d9e6dd3c63d4e729ecd3
Pablo Alan <pabloxt14@gmail.com>
2024-02-20 22:04:33 -0300

    feat: create component horizontal-divider

---
+++ src/components/divider/horizontal.tsx
@@ -0,0 +1,11 @@
+import { cn } from '@/lib/utils'
+
+type HorizontalDividerProps = {
+  className?: string
+}
+
+export const HorizontalDivider = ({ className }: HorizontalDividerProps) => {
+  return (
+    <div className={cn('my-8 w-full border-b border-b-gray-800', className)} />
+  )
+}
