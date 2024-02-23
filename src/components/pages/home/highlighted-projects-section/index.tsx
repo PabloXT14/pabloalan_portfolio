3efd96b11b4445bc36f1760b56d2bbd5ad348708
Pablo Alan <pabloxt14@gmail.com>
2024-02-23 18:19:58 -0300

    feat: finished base highlighted-projects-section

--- src/components/pages/home/highlighted-projects-section/index.tsx
+++ src/components/pages/home/highlighted-projects-section/index.tsx
@@ -1,9 +1,35 @@
+import { HorizontalDivider } from '@/components/divider/horizontal'
 import { SectionTitle } from '@/components/section-title'
+import { ProjectCard } from './project-card'
+import { Button } from '@/components/ui/button'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+import Link from 'next/link'
 
 export const HighlightedProjectsSection = () => {
   return (
     <div className="container py-16">
       <SectionTitle subtitle="destaques" title="Projetos em Destaque" />
+
+      <HorizontalDivider className="mb-16" />
+
+      <div>
+        <ProjectCard />
+        <HorizontalDivider className="my-16" />
+        <ProjectCard />
+        <HorizontalDivider className="my-16" />
+        <ProjectCard />
+        <HorizontalDivider className="my-16" />
+
+        <p className="flex items-center gap-5">
+          <span className="text-gray-400">Se interessou?</span>
+          <Link href="/projects">
+            <Button variant="shadow" className="w-max">
+              Ver todos
+              <HiArrowNarrowRight />
+            </Button>
+          </Link>
+        </p>
+      </div>
     </div>
   )
 }
