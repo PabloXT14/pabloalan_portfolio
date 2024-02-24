ee03202bd884fd3b3773fde50ea16ecd03ed75fa
Pablo Alan <pabloxt14@gmail.com>
2024-02-24 17:46:38 -0300

    feat: add form validation

--- src/components/contact-form-section/index.tsx
+++ src/components/contact-form-section/index.tsx
@@ -1,8 +1,29 @@
+'use client'
+
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import { SectionTitle } from '../section-title'
 import { Button } from '../ui/button'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const contactFormSchema = z.object({
+  name: z.string().min(3).max(100),
+  email: z.string().email(),
+  message: z.string().min(3).max(500),
+})
+
+type ContactFormData = z.infer<typeof contactFormSchema>
 
 export const ContactFormSection = () => {
+  const { register, handleSubmit } = useForm<ContactFormData>({
+    resolver: zodResolver(contactFormSchema),
+  })
+
+  function onSubmit(data: ContactFormData) {
+    console.log(data)
+  }
+
   return (
     <section className="flex items-center justify-center bg-gray-950 px-6 py-16 md:py-32">
       <div className="mx-auto w-full max-w-[420px]">
@@ -12,20 +33,26 @@ export const ContactFormSection = () => {
           className="items-center text-center"
         />
 
-        <form className="mt-12 flex w-full flex-col gap-4">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mt-12 flex w-full flex-col gap-4"
+        >
           <input
+            {...register('name')}
             type="text"
             placeholder="Nome"
-            className="h-14 w-full rounded-lg bg-gray-800 p-4 text-gray-500 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-600"
+            className="h-14 w-full rounded-lg bg-gray-800 p-4 text-gray-50 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-600"
           />
           <input
+            {...register('email')}
             type="email"
             placeholder="E-mail"
-            className="h-14 w-full rounded-lg bg-gray-800 p-4 text-gray-500 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-600"
+            className="h-14 w-full rounded-lg bg-gray-800 p-4 text-gray-50 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-600"
           />
           <textarea
+            {...register('message')}
             placeholder="Mensagem"
-            className="h-[138px] w-full resize-none rounded-lg bg-gray-800 p-4 text-gray-500 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-600"
+            className="h-[138px] w-full resize-none rounded-lg bg-gray-800 p-4 text-gray-50 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-600"
             maxLength={500}
           />
 
