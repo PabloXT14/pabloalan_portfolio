30c96975a2cd5b72971753f18e9401e0a60efb29
Pablo Alan <pabloxt14@gmail.com>
2024-02-28 17:38:36 -0300

    feat: fetching home data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,43 @@ import { HeroSection } from '@/components/pages/home/hero-section'
 import { HighlightedProjectsSection } from '@/components/pages/home/highlighted-projects-section'
 import { KnownTechsSection } from '@/components/pages/home/known-techs-section'
 import { WorkExperienceSection } from '@/components/pages/home/work-experience-section'
+import { fetchHygraphQuery } from '@/utils/fetch-hygraph-query'
 
-export default function Home() {
+const getPageData = async () => {
+  const query = `
+    query PageInfoQuery {
+      page(where: {slug: "home"}) {
+        introduction {
+          raw
+        }
+        introTechnologies {
+          name
+        }
+        profilePicture {
+          url
+        }
+        socials {
+          url
+          iconSvg
+        }
+        knownTechs {
+          iconSvg
+          name
+          startDate
+        }
+      }
+    }
+  `
+  const revalidate = 1000 * 60 * 60 * 24 // 24 hours in milliseconds
+
+  return fetchHygraphQuery(query, revalidate)
+}
+
+export default async function Home() {
+  const response = await getPageData()
+
+  console.log(response)
+
   return (
     <>
       <HeroSection />
