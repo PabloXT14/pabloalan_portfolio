690b8d4c512e4072db4b3b559afbb647a98aa937
Pablo Alan <pabloxt14@gmail.com>
2024-02-29 20:31:07 -0300

    feat: create Project & ProjectSection types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,8 +49,6 @@ const getPageData = async (): Promise<HomePageData> => {
 export default async function Home() {
   const { page: pageData } = await getPageData()
 
-  console.log(pageData)
-
   return (
     <>
       <HeroSection homeInfo={pageData} />
--- src/types/projects.ts
+++ src/types/projects.ts
@@ -1,5 +1,33 @@
+import { RichTextContent } from '@graphcms/rich-text-types'
+
 export type KnownTech = {
   iconSvg: string
   name: string
   startDate: string
+}
+
+export type ProjectSection = {
+  title: string
+  image: {
+    url: string
+  }
+}
+
+export type Project = {
+  slug: string
+  thumbnail: {
+    url: string
+  }
+  title: string
+  shortDescription: string
+  description: {
+    raw: RichTextContent
+  }
+  technologies: KnownTech[]
+  pageThumbnail: {
+    url: string
+  }
+  sections: ProjectSection[]
+  liveProjectUrl?: string
+  githubUrl?: string
 }
