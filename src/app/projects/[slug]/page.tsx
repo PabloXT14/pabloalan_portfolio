0dae35ba7b2cffcc0f63c1d528342015fc441875
Pablo Alan <pabloxt14@gmail.com>
2024-03-03 00:50:32 -0300

    feat: fetch data on project page

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,7 +1,55 @@
 import { ProjectDetailsSection } from '@/components/pages/project/project-details-section'
 import { ProjectShowcaseSection } from '@/components/pages/project/project-showcase-section'
+import { ProjectPageData } from '@/types/page-info'
+import { fetchHygraphQuery } from '@/utils/fetch-hygraph-query'
 
-export default function Project() {
+type ProjectProps = {
+  params: {
+    slug: string
+  }
+}
+
+const getProjectDetails = async (slug: string): Promise<ProjectPageData> => {
+  const query = `
+    query ProjectQuery() {
+      project(where: { slug: "${slug}" }) {
+        pageThumbnail {
+          url
+        }
+        thumbnail {
+          url
+        }
+        sections {
+          title,
+          image {
+            url
+          }
+        }
+        title
+        shortDescription
+        description {
+          raw
+          text
+        }
+        technologies {
+          name
+        }
+        liveProjectUrl
+        githubUrl
+      }
+    }
+  `
+
+  const revalidate = 1000 * 60 * 60 * 24 // 24 hours in milliseconds
+
+  return fetchHygraphQuery(query, revalidate)
+}
+
+export default async function Project({ params: { slug } }: ProjectProps) {
+  const { project } = await getProjectDetails(slug)
+
+  console.log(project)
+
   return (
     <>
       <ProjectDetailsSection />
