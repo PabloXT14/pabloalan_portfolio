46da2349a938e2a4a37ac1463061f8e2167d0637
Pablo Alan <pabloxt14@gmail.com>
2024-03-02 23:11:51 -0300

    feat: use fetched data on experience-item

--- src/components/pages/home/work-experience-section/experience-item.tsx
+++ src/components/pages/home/work-experience-section/experience-item.tsx
@@ -1,7 +1,47 @@
+import { RichText } from '@/components/rich-text'
 import { Badge } from '@/components/ui/badge'
+import { WorkExperience } from '@/types/work-experience'
+import { differenceInMonths, differenceInYears, format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import Image from 'next/image'
 
-export const ExperienceItem = () => {
+type ExperienceItemProps = {
+  experience: WorkExperience
+}
+
+export const ExperienceItem = ({ experience }: ExperienceItemProps) => {
+  const {
+    companyLogo,
+    companyName,
+    companyUrl,
+    role,
+    description,
+    technologies,
+  } = experience
+
+  const startDate = new Date(experience.startDate)
+  const endDate = experience.endDate ? new Date(experience.endDate) : null
+
+  const formattedStartDate = format(startDate, 'MMM yyyy', { locale: ptBR })
+  const formattedEndDate = endDate
+    ? format(endDate, 'MMM yyyy', { locale: ptBR })
+    : 'O momento'
+
+  const end = endDate || new Date()
+
+  const months = differenceInMonths(end, startDate)
+  const years = differenceInYears(end, startDate)
+  const monthsRemaining = months % 12
+
+  const formattedDuration =
+    years > 0
+      ? `${years} ano${years > 1 ? 's' : ''} ${
+          monthsRemaining > 0
+            ? ` e ${monthsRemaining} mes${monthsRemaining > 1 ? 'es' : ''}`
+            : ''
+        }`
+      : `${months} mes${months > 1 ? 'es' : ''}`
+
   return (
     <div className="grid grid-cols-[40px,1fr] gap-4 md:gap-10">
       {/* Logo da empresa */}
@@ -10,8 +50,8 @@ export const ExperienceItem = () => {
           <Image
             width={40}
             height={40}
-            src="https://media.graphassets.com/Q4lS2mPkT4Kw3BHM6Ba5"
-            alt="Logo da empresa Workflow"
+            src={companyLogo?.url}
+            alt={`Logo da empresa ${companyName}`}
             className="rounded-full"
           />
         </div>
@@ -23,32 +63,30 @@ export const ExperienceItem = () => {
       <div>
         <div className="flex flex-col gap-2 text-sm sm:text-base">
           <a
-            href="https://www.linkedin.com/company/workwolf/"
+            href={companyUrl}
             target="_blank"
             className="text-gray-500 transition-colors hover:text-sky-500"
           >
-            @ Workflow
+            @ {companyName}
           </a>
-          <h4 className="text-gray-300">Desenvolvedor Front-end</h4>
+          <h4 className="text-gray-300">{role}</h4>
           <span className="text-gray-500">
-            out 2022 • O momento • (1 ano e 4 meses)
+            {formattedStartDate} • {formattedEndDate} • ({formattedDuration})
           </span>
-          <p className="text-gray-400">
-            Desenvolvimento e manutenção de interfaces utilizando React, Next,
-            Tailwind, Typescript e Figma. Para o planejamento dos sprints, é
-            utilizado o Jira.
-          </p>
+          <div className="text-gray-400">
+            <RichText content={description.raw} />
+          </div>
         </div>
 
         <p className="mb-3 mt-6 text-sm font-semibold text-gray-400">
           Competências
         </p>
         <div className="mb-8 flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[350px]">
-          <Badge>React</Badge>
-          <Badge>Next</Badge>
-          <Badge>Tailwind</Badge>
-          <Badge>Typescript</Badge>
-          <Badge>Node.js</Badge>
+          {technologies.map((tech) => (
+            <Badge key={`experience-${companyName}-tech-${tech.name}`}>
+              {tech.name}
+            </Badge>
+          ))}
         </div>
       </div>
     </div>
