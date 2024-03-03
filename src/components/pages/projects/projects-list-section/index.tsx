467a06606c425759b985c8ef2238e8c804a1fcbf
Pablo Alan <pabloxt14@gmail.com>
2024-03-03 07:21:30 -0300

    feat: animate projects-list-section (projects page)

--- src/components/pages/projects/projects-list-section/index.tsx
+++ src/components/pages/projects/projects-list-section/index.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import Link from 'next/link'
 import { ProjectCard } from './project-card'
 import { Project } from '@/types/projects'
+import { motion } from 'framer-motion'
+import { fadeUpAnimation } from '@/lib/animations'
 
 type ProjectsListSectionProps = {
   projects: Project[]
@@ -9,11 +13,17 @@ type ProjectsListSectionProps = {
 export const ProjectsListSection = ({ projects }: ProjectsListSectionProps) => {
   return (
     <section className="container grid grid-cols-1 gap-x-4 gap-y-6 py-32 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))]">
-      {projects.map((project) => {
+      {projects.map((project, i) => {
         return (
-          <Link key={project.slug} href={`/projects/${project.slug}`}>
-            <ProjectCard project={project} />
-          </Link>
+          <motion.div
+            key={project.slug}
+            {...fadeUpAnimation}
+            transition={{ duration: 0.5, delay: i * 0.1 }}
+          >
+            <Link href={`/projects/${project.slug}`}>
+              <ProjectCard project={project} />
+            </Link>
+          </motion.div>
         )
       })}
     </section>
