0b719c5544b58fcdebcceae0f699c742f3ce49a4
Pablo Alan <pabloxt14@gmail.com>
2024-03-03 06:19:08 -0300

    feat: animate KnownTechsSection

--- src/components/pages/home/known-techs-section/index.tsx
+++ src/components/pages/home/known-techs-section/index.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import { SectionTitle } from '@/components/section-title'
 import { KnownTech } from './known-tech'
 import { KnownTech as IKnownTech } from '@/types/projects'
+import { motion } from 'framer-motion'
 
 type KnownTechsSectionProps = {
   techs: IKnownTech[]
@@ -12,8 +15,18 @@ export const KnownTechsSection = ({ techs }: KnownTechsSectionProps) => {
       <SectionTitle title="Conhecimentos" subtitle="competências" />
 
       <div className="mt-16 grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3">
-        {techs.map((tech) => {
-          return <KnownTech key={tech.name} tech={tech} />
+        {techs.map((tech, i) => {
+          return (
+            <motion.div
+              key={tech.name}
+              initial={{ opacity: 0, x: -100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.15, delay: i * 0.1 }}
+            >
+              <KnownTech tech={tech} />
+            </motion.div>
+          )
         })}
       </div>
     </section>
