bde5b4140d7166449dab5614c569de502804f58b
Pablo Alan <pabloxt14@gmail.com>
2024-03-03 07:46:34 -0300

    feat: animate project-showcase-section (project page)

--- src/components/pages/project/project-showcase-section/index.tsx
+++ src/components/pages/project/project-showcase-section/index.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import { ProjectSection } from '@/types/projects'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
+import { fadeUpAnimation } from '@/lib/animations'
 
 type ProjectShowcaseSectionProps = {
   sections: ProjectSection[]
@@ -11,9 +15,11 @@ export const ProjectShowcaseSection = ({
   return (
     <section className="container my-12 flex flex-col gap-8 md:my-32 md:gap-32">
       {sections.map((section) => (
-        <div
+        <motion.div
           key={section.title}
           className="flex flex-col items-center gap-6 md:gap-12"
+          {...fadeUpAnimation}
+          transition={{ duration: 0.5 }}
         >
           <h2 className="text-2xl font-medium text-gray-300 md:text-3xl">
             {section.title}
@@ -27,7 +33,7 @@ export const ProjectShowcaseSection = ({
             className="aspect-auto w-full rounded-lg object-cover"
             unoptimized
           />
-        </div>
+        </motion.div>
       ))}
     </section>
   )
